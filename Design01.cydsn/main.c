016efd64584d8c7baf49dc1a8255d8b863e8dd51
makura46 <makura6706@gmail.com>
2018-02-16 04:16:08 +0900

    インデントを調整

--- Design01.cydsn/main.c
+++ Design01.cydsn/main.c
@@ -8,7 +8,7 @@
  * WHICH IS THE PROPERTY OF your company.
  *
  * ========================================
-*/
+ */
 #include "project.h"
 #include <stdio.h>
 #include <math.h>
@@ -22,16 +22,16 @@
 char out[100]={0};
 void rotate(PS3Controller ps, char na[]) {	// 回転する関数
 	if (ps.L1) {
-        na[0] = 1;
-        na[1] = 1;
-        na[2] = -1;
+		na[0] = 1;
+		na[1] = 1;
+		na[2] = -1;
 		reverse_D_Write(1);
 		reverse_B_Write(1);
 		reverse_C_Write(2);
 	}else {
-        na[0] = -1;
-        na[1] = -1;
-        na[2] = 1;
+		na[0] = -1;
+		na[1] = -1;
+		na[2] = 1;
 		reverse_D_Write(2);
 		reverse_B_Write(2);
 		reverse_C_Write(1);
@@ -39,130 +39,130 @@ void rotate(PS3Controller ps, char na[]) {	// 回転する関数
 	motorD_WriteCompare(200);
 	motorB_WriteCompare(200);
 	motorC_WriteCompare(200);
-    
-    sprintf(out, "%s\n", "aaa");
-    UART_1_PutString(out);
+
+	sprintf(out, "%s\n", "aaa");
+	UART_1_PutString(out);
 }
 
 
 int main(void)
 {
- 	PS3Controller ps3;
-    char i;
+	PS3Controller ps3;
+	char i;
 	double power;
 	uint8 m[3] = {0};
-    char na[3] = {0};
-    char saveRotate[3] = {0};
-    double s;
+	char na[3] = {0};
+	char saveRotate[3] = {0};
+	double s;
 	double x, y;
-    char output[100] = {0};
-    char flag = 0;	// 操作可能かどうか
+	char output[100] = {0};
+	char flag = 0;	// 操作可能かどうか
 
-    CyGlobalIntEnable; /* Enable global interrupts. */
+	CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    motorD_Start();
-    motorB_Start();
-    motorC_Start();
+	/* Place your initialization/startup code here (e.g. MyInst_Start()) */
+	motorD_Start();
+	motorB_Start();
+	motorC_Start();
 	PS3_Start();
-    UART_1_Start();
-    CyDelay(5000);
-    ps3 = PS3_Controller_get();
-    ps3.L2 = 0;
-    ps3.R2 = 0;
-    for(;;)
-    {
+	UART_1_Start();
+	CyDelay(5000);
+	ps3 = PS3_Controller_get();
+	ps3.L2 = 0;
+	ps3.R2 = 0;
+	for(;;)
+	{
 		ps3 = PS3_Controller_get();
 
-        if (flag) {	// flagが立っていれば操作可能
-            if (ps3.CROSS)	// バツを押すと操作を受け付けない
-                flag = !flag;
-    		if (!ps3.L1 && !ps3.R1) {	// L1とR1が押されていなかった場合
-    			
-                
-                //sprintf(output, "%d %d\n", ps3.ANALOG_LX, ps3.ANALOG_LY);
-                //UART_1_PutString(output);
-        		x = ps3.ANALOG_LX - 64;
-        		y = ps3.ANALOG_LY - 64;
-                
-                s = x*x + y*y;
-        		//power = (uint8)(sqrt((double)x * x + y * y));
-                power = sqrt(s);
-                if (power > 64.0)
-                    power *= (64.0 / 90);
-                power /= 64;        // スティックの傾き具合の割合 0 <= power <= 1
-               
-        		matrix(&ps3, m, na);	// 行列計算しmの配列に値を格納  naには回転の向き
-                if (na[0] == 1) {
-        			reverse_D_Write(1);
-        		} else if (na[0] == -1) {
-        			reverse_D_Write(2);
-                } else {
-                    if (saveRotate[0] == 1)
-                        reverse_D_Write(1);
-                    else if (saveRotate[0] == -1)
-                        reverse_D_Write(2);
-                }
-        		if (na[1] == 1) {
-        			reverse_B_Write(1);
-        		} else if (na[1] == -1) {
-        			reverse_B_Write(2);
-                } else {
-                    if (saveRotate[1] == 1)
-                        reverse_B_Write(2);
-                    else if (saveRotate[1] == -1)
-                        reverse_B_Write(1);
-                }
-        		if (na[2] == 1) {
-        			reverse_C_Write(2);
-        		} else if (na[2] == -1) {
-        			reverse_C_Write(1);
-                } else {
-                    if (saveRotate[2] == 1)
-                        reverse_C_Write(1);
-                    else if (saveRotate[2] == -1)
-                        reverse_C_Write(2);
-                }
-        /*
-        		for (i = 0; i < 3; i++) {
-        				m[i] *= MAX_POWER / (double)91 * (double)power;
-        		}
-          */     
-                int max = fmax((double)m[0], fmax((double)m[1], m[2])); 	// mの中で最大のものを探す
-                
-                
-                for (i = 0; i < 3; i++) {
-                    m[i] *= MAX_POWER/max;
-                    
-                    m[i] *= power;	// 最大の値に全部の入力を合わす
-                  
-                }
-                sprintf(output, "\r\n");
-                UART_1_PutString(output);
-                
-                if (m[0] > MAX_POWER || m[1] > MAX_POWER || m[2] > MAX_POWER) {  // 回転の値が最大値を超えると0にする
-                    m[0] = 0;
-                    m[1] = 0;
-                    m[2] = 0;
-                }
-                motorD_WriteCompare(m[0]);
-        		motorB_WriteCompare(m[1]);
-        		motorC_WriteCompare(m[2]);
-               
-                sprintf(output, "%f %f %f    %d %d %d\n", m[0]/255.0, m[1]/255.0, m[2]/255.0, na[0], na[1], na[2]);
-                UART_1_PutString(output);
-            } else if(ps3.L1 || ps3.R1) {	// L1かR1が入力されていると回転する
-                rotate(ps3, na);
-            }
-        } else {
-            if (ps3.CIRCLE)	// 丸を押すと操作を受け付ける
-                flag = !flag;
-        }
-        for (i = 0; i < 3; i++)
-            saveRotate[i] = na[i];
-        
-        /* Place your application code here. */
-    }
+		if (flag) {	// flagが立っていれば操作可能
+			if (ps3.CROSS)	// バツを押すと操作を受け付けない
+				flag = !flag;
+			if (!ps3.L1 && !ps3.R1) {	// L1とR1が押されていなかった場合
+
+
+				//sprintf(output, "%d %d\n", ps3.ANALOG_LX, ps3.ANALOG_LY);
+				//UART_1_PutString(output);
+				x = ps3.ANALOG_LX - 64;
+				y = ps3.ANALOG_LY - 64;
+
+				s = x*x + y*y;
+				//power = (uint8)(sqrt((double)x * x + y * y));
+				power = sqrt(s);
+				if (power > 64.0)
+					power *= (64.0 / 90);
+				power /= 64;        // スティックの傾き具合の割合 0 <= power <= 1
+
+				matrix(&ps3, m, na);	// 行列計算しmの配列に値を格納  naには回転の向き
+				if (na[0] == 1) {
+					reverse_D_Write(1);
+				} else if (na[0] == -1) {
+					reverse_D_Write(2);
+				} else {
+					if (saveRotate[0] == 1)
+						reverse_D_Write(1);
+					else if (saveRotate[0] == -1)
+						reverse_D_Write(2);
+				}
+				if (na[1] == 1) {
+					reverse_B_Write(1);
+				} else if (na[1] == -1) {
+					reverse_B_Write(2);
+				} else {
+					if (saveRotate[1] == 1)
+						reverse_B_Write(2);
+					else if (saveRotate[1] == -1)
+						reverse_B_Write(1);
+				}
+				if (na[2] == 1) {
+					reverse_C_Write(2);
+				} else if (na[2] == -1) {
+					reverse_C_Write(1);
+				} else {
+					if (saveRotate[2] == 1)
+						reverse_C_Write(1);
+					else if (saveRotate[2] == -1)
+						reverse_C_Write(2);
+				}
+				/*
+				   for (i = 0; i < 3; i++) {
+				   m[i] *= MAX_POWER / (double)91 * (double)power;
+				   }
+				 */     
+				int max = fmax((double)m[0], fmax((double)m[1], m[2])); 	// mの中で最大のものを探す
+
+
+				for (i = 0; i < 3; i++) {
+					m[i] *= MAX_POWER/max;
+
+					m[i] *= power;	// 最大の値に全部の入力を合わす
+
+				}
+				sprintf(output, "\r\n");
+				UART_1_PutString(output);
+
+				if (m[0] > MAX_POWER || m[1] > MAX_POWER || m[2] > MAX_POWER) {  // 回転の値が最大値を超えると0にする
+					m[0] = 0;
+					m[1] = 0;
+					m[2] = 0;
+				}
+				motorD_WriteCompare(m[0]);
+				motorB_WriteCompare(m[1]);
+				motorC_WriteCompare(m[2]);
+
+				sprintf(output, "%f %f %f    %d %d %d\n", m[0]/255.0, m[1]/255.0, m[2]/255.0, na[0], na[1], na[2]);
+				UART_1_PutString(output);
+			} else if(ps3.L1 || ps3.R1) {	// L1かR1が入力されていると回転する
+				rotate(ps3, na);
+			}
+		} else {
+			if (ps3.CIRCLE)	// 丸を押すと操作を受け付ける
+				flag = !flag;
+		}
+		for (i = 0; i < 3; i++)
+			saveRotate[i] = na[i];
+
+		/* Place your application code here. */
+	}
 }
 
 /* [] END OF FILE */
--- Design01.cydsn/matrix.c
+++ Design01.cydsn/matrix.c
@@ -7,38 +7,32 @@
 void matrix(PS3Controller *ps3, uint8 motor[], char na[]) {
 	int i, j;
 	float L = 176;
-    char output[100] = {0};
-	//double m[3] = {0}
-    
+
 	double mat[3][3] = {{1.0, 0.0, L}, 
-					  {-1.0/2, -sqrt(3.0)/2, L}, 
-					  {-1.0/2, sqrt(3.0)/2, L}};
-    
+		{-1.0/2, -sqrt(3.0)/2, L}, 
+		{-1.0/2, sqrt(3.0)/2, L}};
+
 	float x = ps3->ANALOG_LX - 64;
 	float y = ps3->ANALOG_LY - 64;
-    float max = fmaxf(fabsf(x), fabsf(y));
-    if (max == 0)
-        max = 1;
+	float max = fmaxf(fabsf(x), fabsf(y));
+	if (max == 0)
+		max = 1;
 	float col[3] = {x, y, 0};
-   
+
 	float re[3] = {0};
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 3; j++) {
 			re[i] += mat[i][j] * col[j];
 		}
 	}
-    
-    
+
 	for (i = 0; i < 3; i++) {
-        if (re[i] > 0)
-            na[i] = 1;
-        else if (re[i] < 0)
-            na[i] = -1;
-        else
-            na[i] = 0;
+		if (re[i] > 0)
+			na[i] = 1;
+		else if (re[i] < 0)
+			na[i] = -1;
+		else
+			na[i] = 0;
 		motor[i] = fabsf(re[i]);
 	}
-   
-    //sprintf(output, "%d %d %d\n", motor[0], motor[1], motor[2]);
-   //UART_1_PutString(output);
 }
